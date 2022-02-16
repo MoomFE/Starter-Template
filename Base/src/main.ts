ae9ab8e93df57595dc0524d71c8d9d875aa8d6ac
倚窗听雨丿醉无心 <994039348@qq.com>
2022-02-16 14:03:05 +0800

    新增 pinia 作为状态管理类库

--- Base/src/main.ts
+++ Base/src/main.ts
@@ -1,12 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/plugins/router';
+import pinia from '@/plugins/pinia';
 
 
 const app = createApp(App);
 
 
 app.use(router);
+app.use(pinia);
 
 
 router.isReady().then(() => {
