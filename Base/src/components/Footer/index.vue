0aed3827a8054bcca52bc00c60508a9703679bba
倚窗听雨丿醉无心 <994039348@qq.com>
2022-02-17 14:48:23 +0800

    新增默认引导页面及相关组件

---
+++ Base/src/components/Footer/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="footer flex justify-center text-2xl text-center mt-6">
+
+    <!-- 回到首页 -->
+    <button @click="router.push({ name: 'Index' })">
+      <vaadin-home-o />
+    </button>
+
+    <!-- 切换深色模式 -->
+    <button @click="theme.toggleDark()">
+      <ic-outline-wb-sunny v-if="theme.dark" />
+      <ph-moon v-else />
+    </button>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { useThemeStore } from '@/store/theme';
+
+  const router = useRouter();
+  const theme = useThemeStore();
+</script>
+
+<style lang="scss">
+  .footer > * {
+    @apply opacity-60 hover:text-primary hover:opacity-100 transition-all mx-2;
+  }
+</style>
---
+++ Base/src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Logo />
+
+  <div class="mt-2">
+    Base Starter Template
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<route lang="yaml">
+  name: Index
+  meta:
+    layout: home
+</route>
