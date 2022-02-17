050e643fb51af1fa48d8c3750684e700750ace1a
倚窗听雨丿醉无心 <994039348@qq.com>
2022-02-17 16:31:26 +0800

    修改 Tailwind 默认字体配置

--- Base/tailwind.config.js
+++ Base/tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
     // 字体
     fontFamily: {
       // 默认字体
-      default: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+      default: ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
       // 等宽字体
       monospace: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace']
     },
