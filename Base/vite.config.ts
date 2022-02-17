84cb907bf1a5de8fee73ece5ebc5d17ed767382f
倚窗听雨丿醉无心 <994039348@qq.com>
2022-02-17 09:19:32 +0800

    添加路径映射 `~` 至 `src` 目录

--- Base/vite.config.ts
+++ Base/vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig(({ mode }) => {
   return {
     resolve: {
       alias: {
+        '~': path.resolve(__dirname, './src'),
         '@': path.resolve(__dirname, './src'),
         '@@': path.resolve(__dirname, './')
       }
