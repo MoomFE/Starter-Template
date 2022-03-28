995004e0cd220e5f586e18fefb30ddc386dce040
倚窗听雨丿醉无心 <994039348@qq.com>
2022-03-28 16:10:39 +0800

    对 Naive UI 主题色进行覆盖

--- Base - Naive UI/src/composables/useNaiveTheme.ts
+++ Base - Naive UI/src/composables/useNaiveTheme.ts
@@ -1,6 +1,7 @@
 import type { GlobalThemeOverrides } from 'naive-ui';
 import { darkTheme, dateZhCN, zhCN } from 'naive-ui';
 import { useThemeStore } from '@/store/theme';
+import { settings } from '@/settings';
 
 export const useNaiveTheme = createSharedComposable(() => {
   const themeStore = useThemeStore();
@@ -9,7 +10,36 @@ export const useNaiveTheme = createSharedComposable(() => {
   const theme = computed(() => themeStore.dark ? darkTheme : null);
   /** 对当前 NaiveUI 主题的变量覆盖 */
   const themeOverrides = computed((): GlobalThemeOverrides => {
-    const overrides = {};
+    const colors = settings.colors;
+    const overrides: GlobalThemeOverrides = {
+      common: {
+        // Primary 颜色
+        primaryColor: colors.primary,
+        primaryColorHover: colors['primary-hover'],
+        primaryColorPressed: colors['primary-active'],
+        primaryColorSuppl: colors.primary,
+        // Info 颜色
+        infoColor: colors.info,
+        infoColorHover: colors['info-hover'],
+        infoColorPressed: colors['info-active'],
+        infoColorSuppl: colors.info,
+        // Success 颜色
+        successColor: colors.success,
+        successColorHover: colors['success-hover'],
+        successColorPressed: colors['success-active'],
+        successColorSuppl: colors.success,
+        // Warning 颜色
+        warningColor: colors.warning,
+        warningColorHover: colors['warning-hover'],
+        warningColorPressed: colors['warning-active'],
+        warningColorSuppl: colors.warning,
+        // Error 颜色
+        errorColor: colors.error,
+        errorColorHover: colors['error-hover'],
+        errorColorPressed: colors['error-active'],
+        errorColorSuppl: colors.error,
+      },
+    };
 
     return overrides;
   });
