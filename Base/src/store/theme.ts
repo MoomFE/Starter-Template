9dc1cf13f73c6611f4a703d26bedb1157eb666f5
倚窗听雨丿醉无心 <994039348@qq.com>
2022-03-28 09:42:11 +0800

    修改 `useThemeStore` 状态管理为组合式方法的方式

--- Base/src/store/theme.ts
+++ Base/src/store/theme.ts
@@ -1,27 +1,13 @@
 import { defineStore } from 'pinia';
 
-/** 本地存储的深色模式状态  */
-const darkStorage = useLocalStorage('dark', false);
+export const useThemeStore = defineStore('theme', () => {
+  /** 深色模式状态 */
+  const dark = useDark();
+  /** 切换深色模式状态 */
+  const toggleDark = useToggle(dark);
 
-// 初始化深色模式状态
-if (darkStorage.value)
-  document.documentElement.classList.add('dark');
-
-export const useThemeStore = defineStore('theme', {
-  state: () => ({
-    /** 深色模式状态 */
-    dark: darkStorage.value,
-  }),
-  actions: {
-    /** 更新深色模式状态 */
-    setDark(dark: boolean) {
-      this.dark = dark;
-      darkStorage.value = dark;
-      document.documentElement.classList.toggle('dark', dark);
-    },
-    /** 切换深色模式状态 */
-    toggleDark() {
-      this.setDark(!this.dark);
-    },
-  },
+  return {
+    dark,
+    toggleDark,
+  };
 });
