808c697eefe4810ce77925c7188e93b422b7a3ea
倚窗听雨丿醉无心 <994039348@qq.com>
2022-03-31 09:17:47 +0800

    优化 Naive 主题配置方式

--- Base - Naive UI/src/composables/useNaiveTheme.ts
+++ Base - Naive UI/src/composables/useNaiveTheme.ts
@@ -1,7 +1,58 @@
 import type { GlobalThemeOverrides } from 'naive-ui';
 import { darkTheme, dateZhCN, zhCN } from 'naive-ui';
+import { deepMerge } from '@moomfe/small-utils';
 import { useThemeStore } from '@/store/theme';
 import { settings } from '@/settings';
+
+/** 项目主题色 */
+const colors = settings.colors;
+
+/** 通用主题变量覆盖 */
+const commonOverrides: GlobalThemeOverrides = {
+  common: {
+    // Primary 颜色
+    primaryColor: colors.primary,
+    primaryColorHover: colors['primary-hover'],
+    primaryColorPressed: colors['primary-active'],
+    primaryColorSuppl: colors.primary,
+    // Info 颜色
+    infoColor: colors.info,
+    infoColorHover: colors['info-hover'],
+    infoColorPressed: colors['info-active'],
+    infoColorSuppl: colors.info,
+    // Success 颜色
+    successColor: colors.success,
+    successColorHover: colors['success-hover'],
+    successColorPressed: colors['success-active'],
+    successColorSuppl: colors.success,
+    // Warning 颜色
+    warningColor: colors.warning,
+    warningColorHover: colors['warning-hover'],
+    warningColorPressed: colors['warning-active'],
+    warningColorSuppl: colors.warning,
+    // Error 颜色
+    errorColor: colors.error,
+    errorColorHover: colors['error-hover'],
+    errorColorPressed: colors['error-active'],
+    errorColorSuppl: colors.error,
+  },
+  Button: {},
+};
+
+/** 亮色主题变量覆盖 */
+const lightThemeOverrides: GlobalThemeOverrides = deepMerge({}, commonOverrides, {
+
+});
+/** 深色主题变量覆盖 */
+const darkThemeOverrides: GlobalThemeOverrides = deepMerge({}, commonOverrides, {
+  Button: {
+    textColorPrimary: '#FFF',
+    textColorHoverPrimary: '#FFF',
+    textColorPressedPrimary: '#FFF',
+    textColorFocusPrimary: '#FFF',
+    textColorDisabledPrimary: '#FFF',
+  },
+});
 
 export const useNaiveTheme = createSharedComposable(() => {
   const themeStore = useThemeStore();
@@ -9,40 +60,7 @@ export const useNaiveTheme = createSharedComposable(() => {
   /** 当前 Naive UI 主题 */
   const theme = computed(() => themeStore.dark ? darkTheme : null);
   /** 对当前 NaiveUI 主题的变量覆盖 */
-  const themeOverrides = computed((): GlobalThemeOverrides => {
-    const colors = settings.colors;
-    const overrides: GlobalThemeOverrides = {
-      common: {
-        // Primary 颜色
-        primaryColor: colors.primary,
-        primaryColorHover: colors['primary-hover'],
-        primaryColorPressed: colors['primary-active'],
-        primaryColorSuppl: colors.primary,
-        // Info 颜色
-        infoColor: colors.info,
-        infoColorHover: colors['info-hover'],
-        infoColorPressed: colors['info-active'],
-        infoColorSuppl: colors.info,
-        // Success 颜色
-        successColor: colors.success,
-        successColorHover: colors['success-hover'],
-        successColorPressed: colors['success-active'],
-        successColorSuppl: colors.success,
-        // Warning 颜色
-        warningColor: colors.warning,
-        warningColorHover: colors['warning-hover'],
-        warningColorPressed: colors['warning-active'],
-        warningColorSuppl: colors.warning,
-        // Error 颜色
-        errorColor: colors.error,
-        errorColorHover: colors['error-hover'],
-        errorColorPressed: colors['error-active'],
-        errorColorSuppl: colors.error,
-      },
-    };
-
-    return overrides;
-  });
+  const themeOverrides = computed(() => themeStore.dark ? darkThemeOverrides : lightThemeOverrides);
 
   return {
     theme,
