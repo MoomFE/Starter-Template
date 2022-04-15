ab4c75f36d1e439e1030929d8f1c059786d4a1cf
倚窗听雨丿醉无心 <994039348@qq.com>
2022-04-15 15:11:21 +0800

    移除 Taro 版本尝试

--- Taro/types/env.d.ts
+++
@@ -1,8 +0,0 @@
-// @ts-expect-error 扩展环境变量
-declare const process: {
-  env: {
-    /** 当前的编译平台类型 */
-    TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt' | 'quickapp' | 'qq'
-    [key: string]: any
-  }
-};
