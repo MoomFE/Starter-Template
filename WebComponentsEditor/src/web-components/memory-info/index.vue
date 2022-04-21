380c37359535fccc2e22885460bb156176376614
倚窗听雨丿醉无心 <994039348@qq.com>
2022-04-21 13:17:11 +0800

    添加测试组件
    未完善内容

---
+++ WebComponentsEditor/src/web-components/memory-info/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    123
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<script lang="ts">
+  import reset from '@unocss/reset/tailwind.css?raw';
+
+  export default {
+    styles: [reset, `@unocss-placeholder`], // eslint-disable-line quotes
+  };
+</script>
+
+<style>
+
+</style>
