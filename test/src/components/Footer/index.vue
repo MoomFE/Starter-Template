6915065a7ba6a3ab01e414ce07c2d30dc8b95ed4
倚窗听雨丿醉无心 <994039348@qq.com>
2022-05-10 15:58:45 +0800

    使用 Base 版本复制出 test

---
+++ test/src/components/Footer/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="footer flex justify-center text-2xl text-center mt-6">
+    <!-- 回到首页 -->
+    <button :title="t('button.home')" @click="router.push({ name: 'Index' })">
+      <i-vaadin-home-o />
+    </button>
+
+    <!-- 切换深色模式 -->
+    <button :title="t('button.toogle-dark-mode')" @click="theme.toggleDark()">
+      <i-ph-moon v-if="theme.dark" />
+      <i-ic-outline-wb-sunny v-else />
+    </button>
+
+    <!-- 切换多语言 -->
+    <button :title="t('button.change-languages')" @click="toggleLocales">
+      <i-mdi-translate />
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { useThemeStore } from '@/stores/theme';
+
+  const router = useRouter();
+  const theme = useThemeStore();
+  const { t, locale, availableLocales: locales } = useI18n();
+
+  function toggleLocales() {
+    locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length];
+  }
+</script>
+
+<style lang="scss">
+  .footer > * {
+    @apply opacity-60 hover:text-primary hover:opacity-100 transition-all mx-2;
+  }
+</style>
