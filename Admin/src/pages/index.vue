a93b3b84398da22d0a2765cfd4f72d53e77f8bb5
倚窗听雨丿醉无心 <994039348@qq.com>
2022-05-14 19:04:09 +0800

    移除无用页面, 开始写登录页面

--- Admin/src/pages/index.vue
+++ Admin/src/pages/index.vue
@@ -1,40 +1,11 @@
 <template>
   <Logo />
-
-  <p class="mt-2">
-    基础项目模板 - Naive UI
-  </p>
-
-  <div class="mt-6">
-    <div class="w-62.5 max-w-full mx-auto">
-      <NInput v-model:value="name" placeholder="你的名字是？" @keydown.enter="go" />
-    </div>
-  </div>
-
-  <div class="mt-3">
-    <NButton type="primary" :disabled="!name" @click="go">
-      确定
-    </NButton>
-  </div>
 </template>
 
 <script lang="ts" setup>
-  const router = useRouter();
 
-  const name = ref('');
-
-  function go() {
-    router.push({
-      name: 'Hello',
-      params: {
-        name: encodeURIComponent(name.value),
-      },
-    });
-  }
 </script>
 
 <route lang="yaml">
 name: Index
-meta:
-  layout: home
 </route>
---
+++ Admin/src/pages/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex">
+    <div class="w-full min-h-screen items-center justify-center" un:flex="~ grow">
+      <div class="w-120 max-w-full" un:p="x-3 y-6">
+        <!-- Logo -->
+        <div class="flex justify-center items-center gap-5">
+          <Logo class="!mx-0" />
+          <div class="text-3xl">{{ settings.title }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { settings } from '@/settings';
+
+</script>
+
+<route lang="yaml">
+name: Login
+</route>
