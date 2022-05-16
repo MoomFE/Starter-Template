12570e134bfccad422632a3c0879070f57206a16
倚窗听雨丿醉无心 <994039348@qq.com>
2022-05-16 13:37:15 +0800

    创建组件测试数据支持使用函数返回测试数据

--- WebComponentsEditor/src/pages/frame/index.vue
+++ WebComponentsEditor/src/pages/frame/index.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script lang="ts" setup>
+  import { isFunction } from '@moomfe/small-utils';
   import { type Tab } from '../index/type';
   import { components } from '@/shared/components';
 
@@ -15,8 +16,15 @@
   const tabs = useLocalStorage<Tab[]>('st-tabs', []);
   /** 当前选项卡信息 */
   const tab = computed(() => tabs.value.find(tab => tab.id === window.name));
-  /** 当前选项卡测试数据 */
-  const data = computed(() => components[tab.value?.component as string]?.data?.[tab.value?.data as string] || {});
+  /** 当前选项卡测试数据 - 支持使用函数返回测试数据 */
+  const data = computed(() => {
+    const result = components[tab.value?.component as string]?.data?.[tab.value?.data as string] || {};
+
+    if (isFunction(result))
+      return result();
+
+    return result;
+  });
 
   // 加载组件
   watchEffect(() => {
