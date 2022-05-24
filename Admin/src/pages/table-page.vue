1993333ef53b55f93363911694b4422f29319a75
倚窗听雨丿醉无心 <994039348@qq.com>
2022-05-24 18:53:56 +0800

    [Admin] 创建表格示例页面

---
+++ Admin/src/pages/table-page.vue
@@ -0,0 +1,10 @@
+<template>
+  <router-view />
+</template>
+
+<route lang="yaml">
+name: TablePage
+meta:
+  layout: admin
+  title: 表格页面
+</route>
---
+++ Admin/src/pages/table-page/basic.vue
@@ -0,0 +1,17 @@
+<template>
+  <TableFrame>
+    <n-data-table
+      class="size-full" flex-height
+    />
+  </TableFrame>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<route lang="yaml">
+name: TablePage/Basic
+meta:
+  title: 基础表格
+</route>
