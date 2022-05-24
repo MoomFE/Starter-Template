9aeee643696354b78fbd0f8503d052dbdaa01fa2
倚窗听雨丿醉无心 <994039348@qq.com>
2022-05-24 18:53:29 +0800

    [Admin] 新增 TableFrame 组件, 用于创建一个表格页面容器

---
+++ Admin/src/components/TableFrame/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="size-full p-2.5">
+    <n-card class="size-full" content-style="padding: 0" :bordered="false">
+      <div class="size-full p-4" un:flex="~ col gap-4">
+        <!-- 头部区域 -->
+        <div v-if="slot.header">
+          <slot name="header" />
+        </div>
+        <!-- 表格区域 -->
+        <div class="size-full flex-grow">
+          <slot />
+        </div>
+      </div>
+    </n-card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const slot = useSlots();
+</script>
+
+<style lang="scss" scoped>
+  // 居中分页
+  :deep(.n-data-table) .n-data-table__pagination {
+    @apply justify-center;
+  }
+</style>
