96f6f93db7c2c217c0ea8e5e7947b85d39b307b3
倚窗听雨丿醉无心 <994039348@qq.com>
2022-06-06 17:20:46 +0800

    [WebComponentsEditor] 插件更换一种方式识别开发环境

--- WebComponentsEditor/scripts/plugins/virtual-public.ts
+++ WebComponentsEditor/scripts/plugins/virtual-public.ts
@@ -1,23 +1,19 @@
 import { resolve } from 'path';
 import { type PluginOption, normalizePath } from 'vite';
 
-interface VirtualPublicOption {
-  /** 当前模式 */
-  mode: string
-}
-
 /**
  * 对 Web Components 组件使用的 public 资源进行虚拟路径替换
  */
-export default function VirtualPublic(options: VirtualPublicOption): PluginOption {
+export default function VirtualPublic(): PluginOption {
   const virtualModuleId = 'virtual:public';
-  const isDev = options.mode === 'development';
-  const dir = normalizePath(
-    resolve(process.cwd(), './src/web-components'),
-  );
+  const dir = normalizePath(resolve(process.cwd(), './src/web-components'));
+  let isDev = true;
 
   return {
     name: 'virtual-public',
+    config(_, { mode }) {
+      isDev = mode === 'development';
+    },
     resolveId(id, source) {
       if (source?.startsWith(dir) && id.startsWith(virtualModuleId)) {
         if (isDev) {
