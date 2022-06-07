db7a85d2106efbed3ea9eff8fced6bfe9033d271
倚窗听雨丿醉无心 <994039348@qq.com>
2022-06-07 11:12:56 +0800

    [Admin] 更改排序规则, 路由未设定 sort 字段将排在最后面

--- Admin/src/modules/router/utils.ts
+++ Admin/src/modules/router/utils.ts
@@ -49,5 +49,5 @@ function sort(a: RouteRecordRaw, b: RouteRecordRaw) {
   const aInfo = isRootRoute(a) ? a.children![0] : a;
   const bInfo = isRootRoute(b) ? b.children![0] : b;
 
-  return (aInfo.meta?.sort as number || 0) - (bInfo.meta?.sort as number || 0);
+  return (aInfo.meta?.sort as number ?? Infinity) - (bInfo.meta?.sort as number ?? Infinity);
 }
