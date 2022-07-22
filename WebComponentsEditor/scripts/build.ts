f444f54f9c91585d201c3ea92778defe939235ca
倚窗听雨丿醉无心 <994039348@qq.com>
2022-07-22 18:15:18 +0800

    [WebComponentsEditor] 优化打包组件的代码, 为后续可选打包做准备

--- WebComponentsEditor/scripts/build.ts
+++ WebComponentsEditor/scripts/build.ts
@@ -1,10 +1,28 @@
 import { resolve } from 'path';
-import fg from 'fast-glob';
 import fs from 'fs-extra';
 import { type InlineConfig, build, mergeConfig } from 'vite';
 import { camelCase } from 'lodash-es';
 import { createViteBaseConfig } from '../vite.config';
-import { formatComponentsGlob } from '../src/utils/formatComponentsGlob';
+
+// Get components info start
+import * as fpsInfoInfo from '@/web-components/fps-info/info';
+import * as memoryInfoInfo from '@/web-components/memory-info/info';
+
+const componentsInfo = {
+  fpsInfo: {
+    name: 'fps-info',
+    info: fpsInfoInfo,
+    indexPath: 'web-components/fps-info/index.ts',
+    viteConfigPath: undefined,
+  },
+  memoryInfo: {
+    name: 'memory-info',
+    info: memoryInfoInfo,
+    indexPath: 'web-components/memory-info/index.ts',
+    viteConfigPath: undefined,
+  },
+} as Record<string, { name: string; info: { name: string; tag: string }; indexPath: string; viteConfigPath?: string }>;
+// Get components info end
 
 const rootPath = resolve(__dirname, '../');
 const srcPath = resolve(rootPath, 'src');
@@ -13,47 +31,29 @@ const outDirPath = resolve(rootPath, 'dist');
 // 清空代码输出目录
 fs.emptyDirSync(outDirPath);
 
-/** 组件注册文件引用 */
-const componentsIndex = formatComponentsGlob(fg.sync(['web-components/*/index.ts'], { cwd: srcPath }));
-/** 组件信息文件 */
-const componentsInfo = formatComponentsGlob(fg.sync(['web-components/*/info.ts'], { cwd: srcPath }));
-/** 组件的额外的 Vite 配置 */
-const componentsViteConfig = formatComponentsGlob(fg.sync(['web-components/*/vite.config.ts'], { cwd: srcPath }));
-
-/** 组件相关信息 */
-const components: Record<string, [string, string]> = {};
-
-// 保存所有的组件相关信息
-Object.entries(componentsInfo).forEach(([name]) => {
-  // index.ts 和 info.ts 必须同时存在, 才承认这是个组件
-  if (!componentsIndex[name]) return;
-
-  components[name] = [
-    componentsIndex[name],
-    componentsViteConfig[name],
-  ];
-});
-
 // 打包所有单个组件
-Object.entries(components).forEach(([name, [path, viteConfigPath]]) => {
-  const viteBaseConfig = createViteBaseConfig();
-  const viteExtraConfig: InlineConfig = {
-    configFile: viteConfigPath ? resolve(srcPath, viteConfigPath) : false,
-    publicDir: resolve(rootPath, 'public', name),
-    build: {
-      minify: true,
-      outDir: resolve(outDirPath, name),
-      emptyOutDir: false,
-      lib: {
-        entry: resolve(srcPath, path),
-        formats: ['iife'],
-        name: camelCase(name),
-        fileName: () => 'index.js',
+(async () => {
+  for (const key in componentsInfo) {
+    const { name, indexPath, viteConfigPath } = componentsInfo[key];
+    const viteBaseConfig = createViteBaseConfig();
+    const viteExtraConfig: InlineConfig = {
+      configFile: viteConfigPath ? resolve(srcPath, viteConfigPath) : false,
+      publicDir: resolve(rootPath, 'public', name),
+      build: {
+        minify: true,
+        outDir: resolve(outDirPath, name),
+        emptyOutDir: false,
+        lib: {
+          entry: resolve(srcPath, indexPath),
+          formats: ['iife'],
+          name: camelCase(name),
+          fileName: () => 'index.js',
+        },
       },
-    },
-  };
+    };
 
-  build(
-    mergeConfig(viteBaseConfig, viteExtraConfig),
-  );
-});
+    await build(
+      mergeConfig(viteBaseConfig, viteExtraConfig),
+    );
+  }
+})();
