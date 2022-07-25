373538dfefd55183e45b133d4f318b4e5573c723
倚窗听雨丿醉无心 <994039348@qq.com>
2022-07-25 11:47:59 +0800

    [WebComponentsEditor] 打包组件时支持可选打包

--- WebComponentsEditor/scripts/build.ts
+++ WebComponentsEditor/scripts/build.ts
@@ -1,7 +1,9 @@
 import { resolve } from 'path';
 import fs from 'fs-extra';
+import prompts from 'prompts';
+import chalk from 'chalk';
 import { type InlineConfig, build, mergeConfig } from 'vite';
-import { camelCase } from 'lodash-es';
+import { camelCase, chunk } from 'lodash-es';
 import { createViteBaseConfig } from '../vite.config';
 
 // Get components info start
@@ -28,32 +30,50 @@ const rootPath = resolve(__dirname, '../');
 const srcPath = resolve(rootPath, 'src');
 const outDirPath = resolve(rootPath, 'dist');
 
-// 清空代码输出目录
-fs.emptyDirSync(outDirPath);
-
 // 打包所有单个组件
 (async () => {
-  for (const key in componentsInfo) {
-    const { name, indexPath, viteConfigPath } = componentsInfo[key];
-    const viteBaseConfig = createViteBaseConfig();
-    const viteExtraConfig: InlineConfig = {
-      configFile: viteConfigPath ? resolve(srcPath, viteConfigPath) : false,
-      publicDir: resolve(rootPath, 'public', name),
-      build: {
-        minify: true,
-        outDir: resolve(outDirPath, name),
-        emptyOutDir: false,
-        lib: {
-          entry: resolve(srcPath, indexPath),
-          formats: ['iife'],
-          name: camelCase(name),
-          fileName: () => 'index.js',
-        },
-      },
-    };
+  const response = await prompts({
+    type: 'multiselect',
+    name: 'value',
+    message: '请选择需要打包的组件',
+    choices: Object.values(componentsInfo).map(info => ({ title: `${info.info.name} ( ${info.info.tag} )`, value: info.name })),
+    instructions: false,
+  });
 
-    await build(
-      mergeConfig(viteBaseConfig, viteExtraConfig),
+  if (response.value.length) {
+    // 清空代码输出目录
+    await fs.emptyDir(outDirPath);
+
+    // 打包组件
+    for (const key of response.value) {
+      const { name, info, indexPath, viteConfigPath } = Object.values(componentsInfo).find(info => info.name === key)!;
+      const viteBaseConfig = createViteBaseConfig();
+      const viteExtraConfig: InlineConfig = {
+        configFile: viteConfigPath ? resolve(srcPath, viteConfigPath) : false,
+        publicDir: resolve(rootPath, 'public', name),
+        build: {
+          minify: true,
+          outDir: resolve(outDirPath, name),
+          emptyOutDir: false,
+          lib: {
+            entry: resolve(srcPath, indexPath),
+            formats: ['iife'],
+            name: camelCase(name),
+            fileName: () => 'index.js',
+          },
+        },
+      };
+
+      console.log(chalk.green(`- 开始打包 ${info.name} ( ${info.tag} ) 组件`));
+
+      await build(
+        mergeConfig(viteBaseConfig, viteExtraConfig),
+      );
+    }
+  }
+  else {
+    console.log(
+      chalk.red('没有选择任何组件'),
     );
   }
 })();
