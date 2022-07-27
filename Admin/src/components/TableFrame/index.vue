ecd18a551249b3da0eda266ef3a4ef2f12029cf9
倚窗听雨丿醉无心 <994039348@qq.com>
2022-07-27 18:09:24 +0800

    [Admin] 重写 `TableFrame` 组件, 支持传入插槽至 `BasicCard` 组件, 新增 `cardProps` 参数

--- Admin/src/components/TableFrame/index.vue
+++ Admin/src/components/TableFrame/index.vue
@@ -1,20 +1,45 @@
-<template>
-  <BasicCard class="size-full" :card-props="{ class: 'size-full' }">
-    <div class="size-full p-3" un:flex="~ col gap-3">
-      <!-- 头部区域 -->
-      <div v-if="slot.header">
-        <slot name="header" />
-      </div>
-      <!-- 表格区域 -->
-      <div class="size-full flex-grow">
-        <slot />
-      </div>
-    </div>
-  </BasicCard>
-</template>
+<script lang="tsx" setup>
+  import type { CardProps } from 'naive-ui';
+  import BasicCard from '@/components/BasicCard/index.vue';
 
-<script lang="ts" setup>
-  const slot = useSlots();
+  interface Props {
+    cardProps?: CardProps & Record<string, any>
+  }
+
+  defineProps<Props>();
+</script>
+
+<script lang="tsx">
+  export default defineComponent({
+    render() {
+      const slots = this.$slots;
+      const cardSlots = Object.keys(slots).filter(key => key.startsWith('card-'));
+
+      return (
+        <BasicCard class="size-full" card-props={{ class: 'size-full', ...this.cardProps }}>
+          {
+            Object.assign(
+              // 传递给 <BasicCard /> 的插槽
+              Object.fromEntries(cardSlots.map(key => [key.slice(5), slots[key]])),
+              // 默认插槽内容
+              {
+                default: () => (
+                  <div class="size-full p-3" un:flex="~ col gap-3">
+                    {/* 头部区域 */}
+                    {slots.header && (
+                      <div>{{ default: slots.header }}</div>
+                    )}
+                    {/* 表格区域 */}
+                    <div class="size-full flex-grow">{{ default: slots.default }}</div>
+                  </div>
+                ),
+              },
+            )
+          }
+        </BasicCard>
+      );
+    },
+  });
 </script>
 
 <style lang="scss" scoped>
